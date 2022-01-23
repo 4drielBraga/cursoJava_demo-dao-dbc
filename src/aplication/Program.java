3aec7986f8bad78343277e95b3d1fffdc1bd6b02
Adriel Braga <marcos.adrielbraga@gmail.com>
2022-01-23 15:05:11 -0400

    add seller entitie

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,9 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,13 @@ public class Program {
 
 		Department objDepartment = new Department(1,"Books");
 		
+		Seller objSeller = new Seller(21,"Bob","bob@gmail.com", new Date(),3000.0,objDepartment);
+		
 		System.out.println(objDepartment);
+		
+		System.out.println(objSeller);
+		
+		
 
 	}
 
